e2c8764e854291caf9a76e20fdd0cdbf8c908eed
eytan <edaniyalzade@walmartlabs.com>
2016-05-27 18:15:40 -0400

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Django CSV Export',
-    version='0.0.1',
+    version='0.0.2',
     packages=['django_csv_export',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
